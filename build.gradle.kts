7187ebb9b07c101db016aef11330ea90298e3124
vlad20012 <beskvlad@gmail.com>
2021-01-27 13:23:04 +0300

    Rename "proc_macro_expander" to "intellij-rust-native-helper"

--- build.gradle.kts
+++ build.gradle.kts
@@ -117,7 +117,7 @@ allprojects {
         }
 
         val compileNativeCode = task<Exec>("compileNativeCode") {
-            workingDir = rootDir.resolve("proc_macro_expander")
+            workingDir = rootDir.resolve("intellij-rust-native-helper")
             executable = "cargo"
             // Hack to use unstable `--out-dir` option work for stable toolchain
             // https://doc.rust-lang.org/cargo/commands/cargo-build.html#output-options
